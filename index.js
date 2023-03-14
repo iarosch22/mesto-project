447e01f867433899a6663a99c9cf6229464a421e
Vitaliy Yarash <vit.iarosch@yandex.by>
2023-03-14 12:08:06 +0300

    add close func

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ let userbox = container.querySelector('.userbox');
 let popup = container.querySelector('.popup');
 
 let editBtn = userbox.querySelector('.userbox__edit-btn');
+let closeBtn = popup.querySelector('.popup__close-btn');
 
-editBtn.addEventListener('click', openPopup)
+
+editBtn.addEventListener('click', openPopup);
+closeBtn.addEventListener('click', closePopup);
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
+}
+function closePopup() {
+    popup.classList.remove('popup_opened');
 }
