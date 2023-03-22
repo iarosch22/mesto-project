05768a6927af2d175dc09c0ea2007c9402d09217
Vitaliy Yarash <vit.iarosch@yandex.by>
2023-03-22 09:22:57 +0300

    add add-form

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let container = document.querySelector('.page');
+const container = document.querySelector('.page');
 
-let userbox = container.querySelector('.userbox');
-let popup = container.querySelector('.popup');
+const userbox = container.querySelector('.userbox');
+const popup = container.querySelector('.popup');
 
-let editBtn = userbox.querySelector('.userbox__edit-btn');
-let closeBtn = popup.querySelector('.popup__close-btn');
+const editBtn = userbox.querySelector('.userbox__edit-btn');
+const closeBtn = popup.querySelector('.popup__close-btn');
 
 
 editBtn.addEventListener('click', openPopup);
@@ -17,3 +17,26 @@ function openPopup() {
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
+
+const userboxName = userbox.querySelector('.userbox__name');
+const userboxStatus = userbox.querySelector('.userbox__status');
+
+const form = popup.querySelector('.form');
+const nameInput = popup.querySelector('#username');
+const statusInput = popup.querySelector('#status');
+
+nameInput.value = userboxName.textContent;
+statusInput.value = userboxStatus.textContent;
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    const enteredName = nameInput.value;
+    const enteredStatus = statusInput.value;
+
+    userboxName.textContent = enteredName;
+    userboxStatus.textContent = enteredStatus;
+
+    closePopup();
+}
+
+form.addEventListener('submit', handleFormSubmit);
