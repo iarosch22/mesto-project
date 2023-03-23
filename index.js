b818dddcb7bbaf1243febd31ba8be128450cbd30
Vitaliy Yarash <vit.iarosch@yandex.by>
2023-03-23 09:36:50 +0300

    add func for ford-add

--- index.js
+++ index.js
@@ -29,17 +29,18 @@ function closePopup() {
     }
 }
 
+/* form-edit */
 const userboxName = userbox.querySelector('.userbox__name');
 const userboxStatus = userbox.querySelector('.userbox__status');
 
-const form = popupEdit.querySelector('.form');
+const formEdit = popupEdit.querySelector('.form');
 const nameInput = popupEdit.querySelector('#username');
 const statusInput = popupEdit.querySelector('#status');
 
 nameInput.value = userboxName.textContent;
 statusInput.value = userboxStatus.textContent;
 
-function handleFormSubmit(evt) {
+function handleFormEdit(evt) {
     evt.preventDefault();
     const enteredName = nameInput.value;
     const enteredStatus = statusInput.value;
@@ -50,4 +51,38 @@ function handleFormSubmit(evt) {
     closePopup();
 }
 
-form.addEventListener('submit', handleFormSubmit);
+formEdit.addEventListener('submit', handleFormEdit);
+
+/* form-add */
+
+const cardContainer = container.querySelector('.cards');
+
+function addCard(nameValue, urlValue) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').setAttribute('src', urlValue);
+    cardElement.querySelector('.card__image').setAttribute('alt', nameValue);
+    cardElement.querySelector('.card__title').textContent = nameValue;
+
+    cardContainer.append(cardElement);
+}
+
+const fordAdd = popupAdd.querySelector('.form');
+const nameValue = popupAdd.querySelector('#placename');
+const urlValue = popupAdd.querySelector('#url-place');
+
+nameValue.value = nameValue.textContent;
+urlValue.value = urlValue.textContent;
+
+function handleFormAdd(evt) {
+    evt.preventDefault();
+    const enteredPlacename = nameValue.value;
+    const enteredUrl = urlValue.value;
+
+    addCard(enteredPlacename, enteredUrl);
+
+    closePopup();
+}
+
+fordAdd.addEventListener('submit', handleFormAdd)
